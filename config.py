e4d5d8f67f20623ae047467dcf1bd9dba1967af4
IanIcebear <61924692+IanIcebear@users.noreply.github.com>
2023-06-07 11:42:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ DEBUG = True
 
 # #读取数据库环境变量
 username = os.environ.get("MYSQL_USERNAME", 'root')
-password = os.environ.get("MYSQL_PASSWORD", '+1sYZH466105')
+password = os.environ.get("MYSQL_PASSWORD", 'root')
 db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
 
 # username = os.environ.get("MYSQL_USERNAME", 'root')
